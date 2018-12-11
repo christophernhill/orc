ac36016a0e4500edfeb1e6da41add07be0b3374e
Kenan Erdogan <kenan.erdogan@gesis.org>
2018-12-11 08:15:04 +0100

    new nfs provisioner with xfs quota feature for user storage

--- nfs_provisioner/README.md
+++ nfs_provisioner/README.md
@@ -1,5 +1,5 @@
-Files (`rbac.yaml`, `psp.yaml`, `deployment.yaml` and `class.yaml`) are based on 
-[NFS provisioner (v2.2.0)](https://github.com/kubernetes-incubator/external-storage/tree/nfs-provisioner-v2.2.0-k8s1.12/nfs) repo.
+Based on [NFS provisioner (v2.2.0)](https://github.com/kubernetes-incubator/external-storage/tree/nfs-provisioner-v2.2.0-k8s1.12/nfs) repo.
 
-`nfs-provisioner` pod is assigned to a specific node where storage (`/srv/nfs-provisioner`) and
-backup (`/backup/nfs-backup`) disks are mounted.
+There are 2 nfs provisioners in this project:
+- `xfs-nfs-sc` is to be used for user PVs (it has project (user PV folder) based xfs quota).
+- `nfs-sc` is the default storage class which is used everything except user PVs (e.g. prometheus, grafana data).
---
+++ nfs_provisioner/xfs/quota/quota.py
@@ -0,0 +1,58 @@
+from datetime import datetime
+from subprocess import run, PIPE
+from os.path import basename, join
+
+
+def quota():
+    """
+    This script is called every time nfs-provisioner projects file is changed.
+
+    According to the change in the projects file, adds into /deletes from xfs quota.
+    """
+    xfs_dir = '/xfs'
+    nfs_dir = f'{xfs_dir}/nfs'
+    # projects file from xfs-nfs-provisioner
+    nfs_projects_path = f'{nfs_dir}/projects'
+    print(f'{datetime.utcnow()} [UTC]: {nfs_projects_path} is changed')
+    # projects file of quota.py
+    projects_path = '/home/iuser/nfs/projects'
+
+    with open(nfs_projects_path, 'r') as f1, \
+         open(projects_path, 'r+') as f2:
+        nfs_projects = {l for l in f1.read().splitlines() if l.strip()}
+        projects = {l for l in f2.read().splitlines() if l.strip()}
+
+        deleted = projects.difference(nfs_projects)
+        for d in deleted:
+            print(f'{datetime.utcnow()} [UTC]: deleted project: {d}')
+            project_id = d.split(':')[0]
+            # remove project from xfs quota by setting hard limit to 0
+            cp = run(f"xfs_quota -x -c 'limit -p bhard=0 {project_id}' {xfs_dir}",
+                     shell=True, stdout=PIPE, stderr=PIPE)
+            print('{} - {} - {} - {}'.format(cp.args, cp.returncode, cp.stdout, cp.stderr))
+            projects.remove(d)
+
+        added = nfs_projects.difference(projects)
+        for a in added:
+            print(f'{datetime.utcnow()} [UTC]: added project: {a}')
+            project_id, project_path, hard_limit = a.split(':')
+            project_folder = basename(project_path)
+            project_path = join(nfs_dir, project_folder)
+            # add new project to xfs quota
+            cp = run(f"xfs_quota -x -c 'project -s -p {project_path} {project_id}' f'{xfs_dir}",
+                     shell=True, stdout=PIPE, stderr=PIPE)
+            print('{} - {} - {} - {}'.format(cp.args, cp.returncode, cp.stdout, cp.stderr))
+            # set hard limit for this project
+            cp = run(f"xfs_quota -x -c 'limit -p bhard={hard_limit} {project_id}' f'{xfs_dir}",
+                     shell=True, stdout=PIPE, stderr=PIPE)
+            print('{} - {} - {} - {}'.format(cp.args, cp.returncode, cp.stdout, cp.stderr))
+            projects.add(a)
+
+        # re-write all projects into projects file of quota.py
+        f2.seek(0)
+        f2.write('\n'.join(projects) + '\n')
+        f2.truncate()
+
+
+if __name__ == '__main__':
+    quota()
